106ee1cb6b3e71ab6e89ed958d29db96da23c971
gatita <hatamyarg@gmail.com>
2015-03-16 10:42:22 -0700

    session01

---
+++ session01/break_me.py
@@ -0,0 +1,13 @@
+def nameless():
+    return x+y
+    
+def wrongType():
+    return "two cats" + 2
+    
+def typo()
+   return 2+2
+
+def attributeFail():
+    x = {"you": "me"}
+    x.append("everyone")
+    return x
