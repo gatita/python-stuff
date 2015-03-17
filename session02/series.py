1a0379898a97346868590605cbbcaae203f52544
gatita <hatamyarg@gmail.com>
2015-03-17 14:08:43 -0700

    added lucas to series module

--- session02/series.py
+++ session02/series.py
@@ -19,4 +19,26 @@ def fibRecursive(n):
         return 1
     else:
         return fibRecursive(n-1) + fibRecursive(n-2)
+        
+def lucas(n):
+    """Return the nth number in the Lucas sequence"""
+    f1, f2 = 2, 1
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    for i in range(n-1):
+        value = f1 + f2
+        f1 = f2
+        f2 = value
+    return value
+    
+def fibLucas(n):
+    """Return the nth number in the Lucas sequence recursively"""
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return fibLucas(n-1) + fibLucas(n-2)
     
