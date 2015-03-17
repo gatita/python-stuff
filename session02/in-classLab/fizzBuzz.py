0e51958a1cd3cc5349147242f440cdb1f08f7995
gatita <hatamyarg@gmail.com>
2015-03-16 18:58:29 -0700

    session02

---
+++ session02/in-classLab/fizzBuzz.py
@@ -0,0 +1,28 @@
+def fizzBuzz():
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print 'FizzBuzz'
+        elif i % 3 == 0:
+            print 'Fizz'
+        elif i % 5 == 0:
+            print 'Buzz'
+        else:
+            print i
+            
+def fizzBuzz2():
+    for i in range(1,101):
+        if not i%3 and not i%5:
+            print 'FizzBuzz'
+        elif not i%3:
+            print 'Fizz'
+        elif not i%5:
+            print 'Buzz'
+        else:
+            print i
+            
+def fizzBuzz3():
+    print [(not i%3)*'Fizz' + (not i%5)*'Buzz' or i for i in range(1,101)]
+
+
+    
+    
