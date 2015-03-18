fdf0093cf92a0e117e98e6e5b4e16a1b27907a39
gatita <hatamyarg@gmail.com>
2015-03-17 18:41:29 -0700

    added assert statements to series.py to test the functions, fibonacci, lucas, and sum_series

--- session02/series.py
+++ session02/series.py
@@ -24,7 +24,7 @@ def lucas(n):
     """Return the nth number in the Lucas sequence"""
     f1, f2 = 2, 1
     if n == 0:
-        return 0
+        return 2
     elif n == 1:
         return 1
     for i in range(n-1):
@@ -68,3 +68,37 @@ def sum_series(n, x=0, y=1):
         f1 = f2
         f2 = value
     return value
+    
+if __name__ == "__main__":
+    #test fibonacci 
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
+    assert fibonacci(5) == 5
+    assert fibonacci(6) == 8
+    assert fibonacci(7) == 13
+    
+    #test lucas
+    assert lucas(0) == 2
+    assert lucas(1) == 1
+    assert lucas(2) == 3
+    assert lucas(3) == 4
+    assert lucas(4) == 7
+    assert lucas(5) == 11
+    assert lucas(6) == 18
+    assert lucas(7) == 29
+    
+    #test sum_series
+    assert sum_series(0) == 0
+    assert sum_series(1) == 1
+    assert sum_series(2) == 1
+    assert sum_series(3) == 2
+    assert sum_series(4) == 3
+    assert sum_series(7) == 13
+    assert sum_series(0, 2, 1) == 2
+    assert sum_series(1, 2, 1) == 1
+    assert sum_series(2, 2, 1) == 3
+    assert sum_series(3, 2, 1) == 4
+    assert sum_series(7, 2, 1) == 29
