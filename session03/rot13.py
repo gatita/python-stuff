39457f7ddc8663511ee1f3258b1575aacd58dbb1
gatita <hatamyarg@gmail.com>
2015-03-22 16:04:37 -0700

    added rot13 function which takes any text and returns that text encrypted by ROT13

--- session03/rot13.py
+++ session03/rot13.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python
+
+from string import maketrans
+
+def rot13(text):
+    original = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    cyphered = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+    table = maketrans(original, cyphered)
+    return text.translate(table)
