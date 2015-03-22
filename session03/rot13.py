268998be755c095924d8b850f9e6616319bd6e70
gatita <hatamyarg@gmail.com>
2015-03-22 16:11:34 -0700

    added tests of rot13

--- session03/rot13.py
+++ session03/rot13.py
@@ -7,3 +7,11 @@ def rot13(text):
     cyphered = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
     table = maketrans(original, cyphered)
     return text.translate(table)
+
+#test rot13 encryption
+if __name__ == '__main__':
+    assert rot13('hi') == 'uv'
+    assert rot13('HI') == 'UV'
+    assert rot13('Hey there, friend!') == ('Url gurer, sevraq!')
+    assert rot13(rot13('hi')) == 'hi'
+    print 'Looks good'
