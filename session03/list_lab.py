3f0857384e898781817308e4fe94bdc6a7d2a9e2
gatita <hatamyarg@gmail.com>
2015-03-22 12:46:57 -0700

    enclosed series1 in a function

--- session03/list_lab.py
+++ session03/list_lab.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 
 fruit = ["Apples", "Pears", "Oranges", "Peaches"]
-print fruit
-new_fruit = raw_input("What's another type of fruit? ")
-fruit.append(new_fruit)
-print fruit
-number = raw_input("Pick a number, 1 through %i. " % len(fruit))
-print number + ': ' + fruit[int(number)-1]
-fruit = ['Grapes'] + fruit
-print fruit
-fruit.insert(0, 'Kiwi')
-print fruit
 
-for x in fruit:
-    if x[0] == 'P':
-        print x
+def series1(fruit):
+    fruit_copy = fruit[:]
+    print fruit
+    new_fruit = raw_input("What's another type of fruit? ")
+    fruit_copy.append(new_fruit)
+    print fruit_copy
+    number = raw_input("Pick a number, 1 through %i. " % len(fruit_copy))
+    print number + ': ' + fruit_copy[int(number)-1]
+    fruit_copy = ['Grapes'] + fruit_copy
+    print fruit_copy
+    fruit_copy.insert(0, 'Kiwi')
+    print fruit_copy
+
+    for x in fruit_copy:
+        if x[0] == 'P':
+            print x
+            
+    return fruit_copy
+
+foo = series1(fruit)
+
+
+
