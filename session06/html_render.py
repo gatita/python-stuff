464aafe574a7889eccb5d01d9faa617c0ee6fd79
gatita <hatamyarg@gmail.com>
2015-04-28 20:56:37 -0700

    step 3

--- session06/html_render.py
+++ session06/html_render.py
@@ -22,14 +22,29 @@ class Element(object):
                 file_out.write(ind + unicode(child)+'\n')
         file_out.write('\n' + ind + u'</%s>' % self.tag)
 
-
 class Html(Element):
     tag = 'html'
+
+    # def __init__(self, content=None):
+    #     Element.__init__(self, content=None)
 
 class Body(Element):
     tag = 'body'
 
 class P(Element):
     tag = 'p'
+
+class Head(Element):
+    tag = 'head'
+
+class OneLineTag(Element):
+    def render(self, file_out, ind=''):
+        file_out.write('\n' + ind + u'<%s>' % self.tag)
+        file_out.write(unicode(self.children))
+        file_out.write(u'</%s>' % self.tag)
+
+class Title(OneLineTag):
+    tag = 'title'
+
 
 
