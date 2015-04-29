59bc2a804bdae7357b1315169df5840883dc0034
gatita <hatamyarg@gmail.com>
2015-04-28 22:35:07 -0700

    step 4

--- session06/html_render.py
+++ session06/html_render.py
@@ -4,22 +4,30 @@ class Element(object):
     tag = 'html'
     indent = '    '
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
         if not content:
             self.children = []
         else:
             self.children = [content]
 
+        self.attributes = kwargs
+
     def append(self, new_content):
         self.children.append(new_content)
 
     def render(self, file_out, ind=''):
-        file_out.write('\n' + ind + u'<%s>' % self.tag)
+        if self.attributes:
+            file_out.write('\n' + ind + u'<%s ' % self.tag)
+            for k, v in self.attributes.items():
+                file_out.write("%s='%s'>" % (k,v))
+        else:
+            file_out.write('\n' + ind + u'<%s>' % self.tag)
+
         for child in self.children:
             try:
                 child.render(file_out, ind + self.indent)
             except AttributeError:
-                file_out.write(ind + unicode(child)+'\n')
+                file_out.write(ind + unicode(child))
         file_out.write('\n' + ind + u'</%s>' % self.tag)
 
 class Html(Element):
