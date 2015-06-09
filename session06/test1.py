d4870142d21b0b1f196006ca2d9dc1f960b7f590
Grace Hatamyar <hatamyarg@gmail.com>
2015-06-09 13:42:28 -0700

    add tests for element content

--- session06/test1.py
+++ session06/test1.py
@@ -2,12 +2,18 @@ import unittest
 
 from html_render import Element
 
-class TestElementRender(unittest.TestCase):
+class TestElementClass(unittest.TestCase):
 
-    def test_create_element(self):
+    def test_create_element_no_content(self):
         page = Element()
         self.assertEqual(page.tag, 'html')
         self.assertEqual(page.indent, '    ')
+        self.assertFalse(page.children)
+
+    def test_create_element_content(self):
+        page = Element(content="Test Content")
+        self.assertEqual(page.children, ["Test Content"])
+
 
 
 
@@ -15,5 +21,5 @@ class TestElementRender(unittest.TestCase):
 # if __name__ == '__main__':
 #     unittest.main()
 
-suite = unittest.TestLoader().loadTestsFromTestCase(TestElementRender)
+suite = unittest.TestLoader().loadTestsFromTestCase(TestElementClass)
 unittest.TextTestRunner(verbosity=2).run(suite)
