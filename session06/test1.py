62349594bbf47c76dd0ad6ceb4def54eefa6a475
Grace Hatamyar <hatamyarg@gmail.com>
2015-06-11 20:09:15 -0700

    add test for append method of Element class

--- session06/test1.py
+++ session06/test1.py
@@ -14,6 +14,36 @@ class TestElementClass(unittest.TestCase):
         page = Element(content="Test Content")
         self.assertEqual(page.children, ["Test Content"])
 
+    def test_append_no_prior_content(self):
+        page = Element()
+        t1 = "One night--it was on the twentieth of March, 1888--I was \
+            returning from a journey to a patient (for I had now returned to \
+            civil practice), when my way led me through Baker Street."
+
+        t2 = "As I passed the well-remembered door, which must always be \
+             associated in my mind with my wooing, and with the dark incidents \
+             of the Study in Scarlet, I was seized with a keen desire to see \
+             Holmes again, and to know how he was employing his extraordinary \
+             powers."
+
+        page.append(t1)
+        page.append(t2)
+
+        self.assertEqual(page.children, [t1, t2])
+
+    def test_append_with_prior_content(self):
+        page = Element(content="Test Content")
+        t1 = "One night--it was on the twentieth of March, 1888--I was \
+            returning from a journey to a patient (for I had now returned to \
+            civil practice), when my way led me through Baker Street."
+
+        page.append(t1)
+        self.assertEqual(page.children, ["Test Content", t1])
+
+    def test_append_non_string(self):
+        page = Element()
+        page.append(2)
+        self.assertEqual(page.children, [2])
 
 
 
@@ -23,3 +53,4 @@ class TestElementClass(unittest.TestCase):
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestElementClass)
 unittest.TextTestRunner(verbosity=2).run(suite)
+
